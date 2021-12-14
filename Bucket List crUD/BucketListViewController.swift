2f99234a9eebb49ce2eab7824acc91addf957984
Mostafa Nafie <mostafa.nafie@robustastudio.com>
2021-12-14 16:32:50 +0200

    Add TableView Datasource

--- Bucket List crUD/BucketListViewController.swift
+++ Bucket List crUD/BucketListViewController.swift
@@ -9,10 +9,35 @@ import UIKit
 
 class BucketListViewController: UITableViewController {
 
+    // MARK: - Variables
+    private var items = ["Sky diving", "Live in Hawaii"]
+
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+        setupTitle()
+    }
 
 }
 
+// MARK: - TableView Datasource
+extension BucketListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+}
+
+// MARK: - Private Methods
+extension BucketListViewController {
+    private func setupTitle() {
+        title = "Bucket List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
